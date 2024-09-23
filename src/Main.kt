33fdc8b226b3135907414e650fd998cf65feb11f
Admin <reteamboss@gmail.com>
2024-09-23 17:02:46 +0300

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    println("Hello World!")
+    var mySet = setOf(1,2,3,4,5,6,7,8,9,10)
+    var mySetOne = mySet.filter { it % 2 == 0 }
+    var mySetTwo = mySet.filter { it % 2 != 0 }
+    println(mySetOne)
+    println(mySetTwo)
+
 }
